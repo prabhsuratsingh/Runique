9caa932d6009846a4bfdebd6f10a714d32fb5dbc
prabhsuratsingh <prabhsurat17@gmail.com>
2025-01-08 23:23:25 +0530

    created feature UI plugin

---
+++ build-logic/convention/src/main/java/AndroidFeatureUiConventionPlugin.kt
@@ -0,0 +1,27 @@
+import com.android.build.api.dsl.LibraryExtension
+import com.example.convention.ExtensionType
+import com.example.convention.addUiLayerDependencies
+import com.example.convention.configureAndroidCompose
+import com.example.convention.configureBuildTypes
+import com.example.convention.configureKotlinAndroid
+import com.example.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.kotlin
+
+class AndroidFeatureUiConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("runique.android.library.compose")
+            }
+
+            dependencies {
+                addUiLayerDependencies(target)
+            }
+        }
+    }
+}
