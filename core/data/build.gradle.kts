12a50ed543e4ba175ce19a27939aba0a5029c178
prabhsuratsingh <prabhsurat17@gmail.com>
2025-01-10 21:58:12 +0530

    created ktor plugin

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     alias(libs.plugins.runique.android.library)
+    alias(libs.plugins.runique.jvm.ktor)
 }
 
 android {
     namespace = "com.example.core.data"
-
 }
 
 dependencies {
