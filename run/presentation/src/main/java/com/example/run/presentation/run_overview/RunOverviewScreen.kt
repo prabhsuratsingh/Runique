6541441960b8743ba948b8805054b77689604016
prabhsuratsingh <prabhsurat17@gmail.com>
2025-03-09 00:09:31 +0530

    Create run overview screen

--- run/presentation/src/main/java/com/example/run/presentation/run_overview/RunOverviewScreen.kt
+++ run/presentation/src/main/java/com/example/run/presentation/run_overview/RunOverviewScreen.kt
@@ -1,8 +1,26 @@
 package com.example.run.presentation.run_overview
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.core.presentation.designsystem.AnalyticsIcon
+import com.example.core.presentation.designsystem.LogoIcon
+import com.example.core.presentation.designsystem.LogoutIcon
+import com.example.core.presentation.designsystem.RunIcon
 import com.example.core.presentation.designsystem.RuniqueTheme
+import com.example.core.presentation.designsystem.components.RuniqueFloatingActionButton
+import com.example.core.presentation.designsystem.components.RuniqueScaffold
+import com.example.core.presentation.designsystem.components.RuniqueToolbar
+import com.example.core.presentation.designsystem.components.util.DropDownItem
+import com.example.run.presentation.R
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -14,11 +32,59 @@ fun RunOverviewScreenRoot(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun RunOverviewScreen(
     onAction: (RunOverviewAction) -> Unit
 ) {
-    
+    val topAppBarState = rememberTopAppBarState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+        topAppBarState
+    )
+
+    RuniqueScaffold(
+        topAppBar = {
+            RuniqueToolbar(
+                showBackButton = false,
+                title = stringResource(R.string.runique),
+                scrollBehavior = scrollBehavior,
+                startContent = {
+                    Icon(
+                        imageVector = LogoIcon,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.size(30.dp)
+                    )
+                },
+                menuItems = listOf(
+                    DropDownItem(
+                        icon = AnalyticsIcon,
+                        title = stringResource(R.string.analytics)
+                    ),
+                    DropDownItem(
+                        icon = LogoutIcon,
+                        title = stringResource(R.string.logout)
+                    )
+                ),
+                onMenuItemClick = {index ->
+                    when(index) {
+                        0 -> onAction(RunOverviewAction.OnAnalyticsCLick)
+                        1 -> onAction(RunOverviewAction.OnLogoutClick)
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            RuniqueFloatingActionButton(
+                icon = RunIcon,
+                onClick = {
+                    onAction(RunOverviewAction.OnStartClick)
+                }
+            )
+        }
+    ) { padding ->
+
+    }
 }
 
 @Preview
